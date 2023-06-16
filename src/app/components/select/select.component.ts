67c00b5e1c9cafbce9b78aadef2bf5a562e2d4bf
Ola Czyrnek <aleksandra@czyrnek.net>
2023-06-16 17:09:30 +0200

    made same with passengers

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { faHome, faCalendarAlt, faMapMarker, faUser, faSuitcase,faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome,
+  faCalendarAlt,
+  faMapMarker,
+  faUser,
+  faSuitcase,
+  faInfoCircle,
+} from '@fortawesome/free-solid-svg-icons';
 import { DestinationService } from '../../services/destination.service';
 import { Destination } from 'src/app/interfaces/destination';
 import { FormsModule } from '@angular/forms';
@@ -7,7 +14,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  styleUrls: ['./select.component.scss'],
 })
 export class SelectComponent implements OnInit {
   minDate: Date = new Date();
@@ -25,6 +32,8 @@ export class SelectComponent implements OnInit {
   selectedOption_children: number | undefined;
   selectedOption_babies: number | undefined;
   passengerOptions: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  passengerCount: number = 0;
+  passengersButtonText: string | undefined;
 
   //Destination input
   destinations: Destination[] = [];
@@ -37,8 +46,8 @@ export class SelectComponent implements OnInit {
   selectedDate: Date | null = null;
 
   //Luggage input
-  luggageOptions: string[] = ["Carry-on", "Carry-on & trolley"]
-  selectedLuggageOption: string | null | undefined
+  luggageOptions: string[] = ['Carry-on', 'Carry-on & trolley'];
+  selectedLuggageOption: string | null = null;
 
   //Submenus config
   isOriginMenuOpen: boolean = false;
@@ -46,15 +55,15 @@ export class SelectComponent implements OnInit {
   isPassengerMenuOpen: boolean = false;
   isLuggageMenuOpen: boolean = false;
 
-
-  constructor(private destinationService: DestinationService) { }
+  constructor(private destinationService: DestinationService) {}
 
   ngOnInit(): void {
-    this.destinationService.getDestinations().subscribe(destinations => {
+    this.destinationService.getDestinations().subscribe((destinations) => {
       this.destinations = destinations;
     });
   }
 
+  // On option click handlers
   onOriginItemClick(destination: Destination): void {
     this.selectedOrigin = destination.desc;
     this.isOriginMenuOpen = false;
@@ -70,6 +79,24 @@ export class SelectComponent implements OnInit {
     this.isLuggageMenuOpen = false;
   }
 
+  onAcceptPassengerSelection(): void {
+    const adultCount = this.selectedOption_adult
+      ? parseInt(this.selectedOption_adult.toString(), 10)
+      : 0;
+    const childrenCount = this.selectedOption_children
+      ? parseInt(this.selectedOption_children.toString(), 10)
+      : 0;
+    const babiesCount = this.selectedOption_babies
+      ? parseInt(this.selectedOption_babies.toString(), 10)
+      : 0;
+
+    this.passengerCount = adultCount + childrenCount + babiesCount;
+    this.isPassengerMenuOpen = false;
+  }
+
+  getSelectedPassengerCount(): number {
+    return this.passengerCount;
+  }
 
   //Submenus handlers
   toggleOriginMenu(): void {
@@ -87,6 +114,4 @@ export class SelectComponent implements OnInit {
   toggleLuggageMenu(): void {
     this.isLuggageMenuOpen = !this.isLuggageMenuOpen;
   }
-
-
 }
